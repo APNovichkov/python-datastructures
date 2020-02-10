204ce30533835363f3607637723f545698cbe6ff
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-02-09 20:53:30 -0800

    [add] Added linkedstack implementation and passes all tests

--- list-abstract-data-types/stack.py
+++ list-abstract-data-types/stack.py
@@ -1,6 +1,6 @@
 #!python
 
-from linkedlist import LinkedList
+from linked_list import LinkedList
 
 
 # Implement LinkedStack below, then change the assignment at the bottom
@@ -21,27 +21,37 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.size == 0
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return self.list.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        self.list.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+
+        if self.is_empty():
+            return None
+
+        return self.list.get_at_index(0)
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+
+        if self.is_empty():
+            raise ValueError("Stack is empty")
+
+        out = self.list.get_at_index(0)
+        self.list.delete(out)
+        return out
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
