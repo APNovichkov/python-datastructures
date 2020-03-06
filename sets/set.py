2fe1c82da6c67819e58b6dd6aa2931591eb69a0b
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-03-06 15:47:52 -0800

    [add] Improved code

--- sets/set.py
+++ sets/set.py
@@ -14,7 +14,7 @@ class Set(object):
     def __str__(self):
         """Return a formatted string representation of this hash table."""
         items = [f"{key}" for key in self.ht.keys()]
-        return ', '.join(items)
+        return f"Set: {', '.join(items)}"
 
     def contains(self, item):
         return self.ht.contains(item)
@@ -25,29 +25,56 @@ class Set(object):
     def remove(self, item):
         self.ht.delete(item)
 
+    def size(self):
+        return self.ht.length()
+
     def union(self, input_set):
+        """Return another set which is the union of self and input set"""
         output_set = Set(self.ht.keys())
         for item in input_set.ht.keys():
             output_set.add(item)
         return output_set
 
     def intersection(self, input_set):
-        output_set = Set()
-        for item in self.ht.keys():
-            if item in input_set.ht.keys():
-                output_set.add(item)
+        """Return another set which is the intersection of self and input set"""
+
+        output_set = Set(self.ht.keys())
+        other_set = input_set
+
+        # Choose the smaller set
+        if input_set.size() < self.size():
+            output_set = input_set
+            other_set = self
+
+        for item in output_set.ht.keys():
+            if not other_set.contains(item):
+                output_set.remove(item)
+
         return output_set
 
     def difference(self, input_set):
+        """Find the difference of self and input set"""
+
         output_set = Set()
         for item in self.ht.keys():
-            if item not in input_set.ht.keys():
+            if not input_set.contains(item):
                 output_set.add(item)
         return output_set
 
     def is_subset(self, input_set):
+        """Return true if self is subset of input_set, false if not
+
+        Best Case: O(1)
+        Worst Case: O(n) where n is # items in self
+        """
+
+        # If self is larger than input set, self can't be subset of input_set
+        if self.size() > input_set.size():
+            return False
+
         for item in self.ht.keys():
-            if item not in input_set.ht.keys():
+            if not input_set.contains(item):
+                # Found at least one item not in self
                 return False
         return True
 
