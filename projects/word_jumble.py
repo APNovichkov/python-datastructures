0cf5ef142a0c5551fe978bb5a22dbfa6dc88bd7b
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-03-07 16:18:31 -0500

    [add] Made improvements to word jumble project

--- projects/word_jumble.py
+++ projects/word_jumble.py
@@ -3,21 +3,33 @@ from log_it import logit
 
 from hashtable import HashTable
 
-def unjumble_word(jumble):
-    logit.log(f"Input jumbled word: {jumble}")
-    jumble = _sort_word(jumble)
-    logit.log(f"Input jumbled word sorted: {jumble}")
+def unjumble_words(jumble):
+    # Get max length of input words
+    max_jumble_length = 0
+    for word in jumble:
+        max_jumble_length = max(max_jumble_length, len(word))
 
-    # Get it's length for optimization later
-    jumble_length = len(jumble)
-
+    # Build hashtable of dictionary words
     dict_path = "/usr/share/dict/words"
-    ht = build_ht(dict_path, jumble_length)
+    ht = build_ht(dict_path, max_jumble_length)
 
-    get_word(jumble, ht)
+    # Run through each input jumble, and unjumble it
+    for word in jumble:
+        word = word.lower()
+        logit.log(f"Input jumbled word: {word}")
+        jumble = _sort_word(word)
+        logit.log(f"Input jumbled word sorted: {word}")
+
+        get_word(jumble, ht)
 
 def get_word(jumble, ht):
+    """Unjumble the input jumble."""
+
     logit.log(f"Trying to unjumble: {jumble}")
+
+    # Since we sorted both the input jumble and all the words in the hashtable, we can just match exactly
+    # and if there is a match, we can get the real word from the hashtable, and if there isnt,
+    # then we say we could not unjumble it
 
     if ht.contains(jumble):
         logit.log(f"Successfuly unjumbled word! -> {ht.get(jumble)}", logit.SUCCESS)
@@ -25,7 +37,7 @@ def get_word(jumble, ht):
         logit.log(f"Unfortunatenly I could not unjumble this word for you: {jumble}", logit.ERROR)
 
 def build_ht(word_list_fp, jumble_length):
-    """Build hashtable of words sorted by character whose total length matches the length of the jumbled word"""
+    """Build hashtable of words sorted by character whose total length is less than or equal to the largest input jumbled word"""
     ht = HashTable()
 
     # Run through it the whole list
@@ -59,7 +71,7 @@ if __name__ == "__main__":
 
     logit.log("Running word jumbler")
 
-    jumble = sys.argv[1].lower()
+    jumble = sys.argv[1:]
 
     # Run app
-    unjumble_word(jumble)
+    unjumble_words(jumble)
